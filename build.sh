3e0d2ab67d3cbce9abe226723fecaf1343ffbc65
connorwk <connork@connorsdomain.com>
2020-07-29 06:14:14 -0400

    Updated build.sh to handle if docker container already exists, if so start it instead.

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
-docker run -d \
-  -it \
-  --name sdl2-build \
-  --mount type=bind,source="$(pwd)",target=/build-path \
-  sdl2-build-env:1.0
+
+if [ -z "$(docker container ls -a | grep sdl2-build)" ]
+then
+	docker run -d \
+	  -it \
+	  --name sdl2-build \
+	  --mount type=bind,source="$(pwd)",target=/build-path \
+	  sdl2-build-env:1.0
+else
+	docker start sdl2-build
+fi
