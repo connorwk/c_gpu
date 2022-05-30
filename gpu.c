bbc823e625f52b535147ec79e68f44c7be8b8c28
Connor Krukosky <connork@connorsdomain.com>
2022-05-30 06:20:55 -0400

    IDK more stuff I guess

--- gpu.c
+++ gpu.c
@@ -5,14 +5,16 @@ SDL_Surface * surf = NULL;
 SDL_Window * uvwind = NULL;
 SDL_Surface * uvsurf = NULL;
 
-struct Camera camera = {
+struct Camera cam = {
     // X, Y, Z
-    20,20,-12,
+    10,0,-20,
     // Pitch, Yaw, Roll
-    -0.7,-0.7,1.57
+    -0.4,0,1.57
 };
 
 float FOV = 1;
+
+float z_depth [SCREEN_WIDTH][SCREEN_HEIGHT] = {0};
 
 int main( int argc, char ** argv )
 {
@@ -46,109 +48,127 @@ int main( int argc, char ** argv )
     SDL_UpdateWindowSurface( uvwind );
 
     struct Triangle test[8] = {
-        {{ // Left
-            {0, 0,  0},
-            {0, 0,  10},
-            {10, 0,  10}
+        {{ // Top
+            {5, -5,  -5},
+            {5, -5,  5},
+            {5, 5,  5}
         }},
         {{
-            {0, 0,  0},
-            {10, 0,  0},
-            {10, 0,  10}
+            {5, -5,  -5},
+            {5, 5,  5},
+            {5, 5,  -5}
+        }},
+        {{ // Left
+            {-5, -5,  -5},
+            {-5, -5,  5},
+            {5, -5,  5}
+        }},
+        {{
+            {-5, -5,  -5},
+            {5, -5,  -5},
+            {5, -5,  5}
         }},
         {{ // Bottom
-            {0, 0,  0},
-            {0, 10,  0},
-            {0, 10,  10}
+            {-5, -5,  -5},
+            {-5, 5,  -5},
+            {-5, 5,  5}
         }},
         {{
-            {0, 0,  0},
-            {0, 10,  10},
-            {0, 0,  10}
+            {-5, -5,  -5},
+            {-5, 5,  5},
+            {-5, -5,  5}
         }},
         {{ // Right
-            {0, 0,  10},
-            {10, 10,  10},
-            {10, 0,  10}
+            {-5, -5,  5},
+            {5, 5,  5},
+            {5, -5,  5}
         }},
         {{
-            {0, 0,  10},
-            {0, 10,  10},
-            {10, 10,  10}
-        }},
-        {{ // Top
-            {10, 0,  0},
-            {10, 0,  10},
-            {10, 10,  10}
-        }},
-        {{
-            {10, 0,  0},
-            {10, 10,  10},
-            {10, 10,  0}
+            {-5, -5,  5},
+            {-5, 5,  5},
+            {5, 5,  5}
         }}
     };
 
     struct TextureTri testuv[8] = {
-        {{
-            {512, 0},
-            {512, 512},
-            {0, 512}
-        }},
-        {{
-            {512, 0},
-            {0, 0},
-            {0, 512}
-        }},
-	{{
-            {0, 0},
-            {512, 512},
-            {512, 0}
-        }},
-        {{
-            {0, 512},
-            {0, 0},
-            {512, 512}
-        }},
-	{{
-            {512, 0},
-            {0, 0},
+    {{
+            {256, 512},
+            {256, 256},
             {512, 512}
         }},
         {{
-            {0, 0},
+            {256, 256},
             {512, 512},
-            {0, 512}
+            {512, 256}
+        }},
+        {{
+            {512, 256},
+            {512, 512},
+            {256, 512}
+        }},
+        {{
+            {512, 256},
+            {256, 256},
+            {256, 512}
         }},
 	{{
-            {0, 512},
-            {0, 0},
+            {256, 256},
+            {512, 512},
+            {512, 256}
+        }},
+        {{
+            {256, 512},
+            {256, 256},
+            {512, 512}
+        }},
+	{{
+            {512, 256},
+            {256, 256},
             {512, 512}
         }},
         {{
-            {0, 0},
+            {256, 256},
             {512, 512},
-            {512, 0}
-        }},
+            {256, 512}
+        }}
     };
 
+    /*
     for ( int i = 0; i < 8; i++ )
     {
         rasterizeTriangle(test + i, testuv + i);
     }
+    */
 
     SDL_UpdateWindowSurface( wind );
 
     int running = 1;
+    float x;
     while ( running )
     {
+        x += 0.01;
+
+        memset(z_depth, 0, sizeof z_depth);
+        SDL_FillRect( surf, NULL, SDL_MapRGB( surf->format, 0, 0, 0 ) );
+        //cam.x = sin(x)*20;
+        cam.y = sin(x)*20;
+        cam.z = -cos(x)*20;
+        //cam.pitch = -x;
+        cam.yaw = -x;
+        //printf("Test: %f %f %f\n", cam.y, cam.z, cam.yaw);
+        for ( int i = 0; i < 8; i++ )
+        {
+            rasterizeTriangle(test + i, testuv + i);
+        }
+        SDL_UpdateWindowSurface( wind );
+
         SDL_Event e;
-        while ( SDL_PollEvent(&e) != 0 )
+        if ( SDL_PollEvent(&e) != 0 )
         {
             if ( e.type == SDL_QUIT ) running = 0;
             if ( e.type == SDL_WINDOWEVENT && e.window.event == SDL_WINDOWEVENT_CLOSE ) running = 0;
         }
-
-        SDL_Delay(100);
+        SDL_Delay(10);
     }
 
     // Cleanup
@@ -157,7 +177,7 @@ int main( int argc, char ** argv )
     return 1;
 }
 
-    void inline setPixel(SDL_Surface * s, int x, int y, uint32_t color) { *(uint32_t *)(s->pixels + y * s->pitch + x * sizeof(uint32_t)) = color; }
+void inline setPixel(SDL_Surface * s, int x, int y, uint32_t color) { *(uint32_t *)(s->pixels + y * s->pitch + x * sizeof(uint32_t)) = color; }
 uint32_t inline getPixel(SDL_Surface * s, int x, int y) { return *(uint32_t *)(s->pixels + y * s->pitch + x * sizeof(uint32_t)); }
 
 struct Vec3 cart2bary(struct Triangle * tri, int x, int y)
@@ -191,17 +211,17 @@ struct Vec3 projectPoint(struct Vec3 Point) {
     struct Vec3 t, c, s, d, out;
     float ez, bx, by;
 
-    t.x = Point.x - camera.x;
-    t.y = Point.y - camera.y;
-    t.z = Point.z - camera.z;
+    t.x = Point.x - cam.x;
+    t.y = Point.y - cam.y;
+    t.z = Point.z - cam.z;
 
-    c.x = cos(camera.pitch);
-    c.y = cos(camera.yaw);
-    c.z = cos(camera.roll);
+    c.x = cos(cam.pitch);
+    c.y = cos(cam.yaw);
+    c.z = cos(cam.roll);
 
-    s.x = sin(camera.pitch);
-    s.y = sin(camera.yaw);
-    s.z = sin(camera.roll);
+    s.x = sin(cam.pitch);
+    s.y = sin(cam.yaw);
+    s.z = sin(cam.roll);
 
     d.x = c.y * (s.z * t.y + c.z * t.x) - s.y * t.z;
     d.y = s.x * (c.y * t.z + s.y * (s.z * t.y + c.z * t.x)) + c.x * (c.z * t.y - s.z * t.x);
@@ -256,69 +276,94 @@ void rasterizeTriangle( struct Triangle * tri, struct TextureTri * uv )
     }
 
     // Re-order Vectors so they go top to bottom in y order.
-    if ( rord.v[0].y > rord.v[1].y ) { pt = rord.v[0]; rord.v[0] = rord.v[1]; rord.v[1] = pt; pu = inUV.v[0]; inUV.v[0] = inUV.v[1]; inUV.v[1] = pu; }
-    if ( rord.v[0].y > rord.v[2].y ) { pt = rord.v[0]; rord.v[0] = rord.v[2]; rord.v[2] = pt; pu = inUV.v[0]; inUV.v[0] = inUV.v[2]; inUV.v[2] = pu; }
-    if ( rord.v[1].y > rord.v[2].y ) { pt = rord.v[1]; rord.v[1] = rord.v[2]; rord.v[2] = pt; pu = inUV.v[1]; inUV.v[1] = inUV.v[2]; inUV.v[2] = pu; }
+    if ( rord.v[0].y > rord.v[1].y ) { pt = rord.v[0]; rord.v[0] = rord.v[1]; rord.v[1] = pt;}
+    if ( rord.v[0].y > rord.v[2].y ) { pt = rord.v[0]; rord.v[0] = rord.v[2]; rord.v[2] = pt;}
+    if ( rord.v[1].y > rord.v[2].y ) { pt = rord.v[1]; rord.v[1] = rord.v[2]; rord.v[2] = pt;}
 
-    float ls, rs, cx;
+    float ls, rs, cx, xzs, yzls, yzrs;
 
+    // Center point
     cx = rord.v[0].x + ((rord.v[1].y - rord.v[0].y)/(rord.v[2].y - rord.v[0].y)) * (rord.v[2].x - rord.v[0].x);
 
     // Draw top half of triangle
+    // Get Left and Right slopes
     if ( rord.v[1].x <= cx )
     {
         ls  = (rord.v[1].x - rord.v[0].x) / (rord.v[1].y - rord.v[0].y);
         rs  = (rord.v[2].x - rord.v[0].x) / (rord.v[2].y - rord.v[0].y);
+        yzls = (rord.v[1].z - rord.v[0].z) / (rord.v[1].y - rord.v[0].y);
+        yzrs = (rord.v[2].z - rord.v[0].z) / (rord.v[2].y - rord.v[0].y);
     }
     else
     {
         rs  = (rord.v[1].x - rord.v[0].x) / (rord.v[1].y - rord.v[0].y);
         ls  = (rord.v[2].x - rord.v[0].x) / (rord.v[2].y - rord.v[0].y);
+        yzrs = (rord.v[1].z - rord.v[0].z) / (rord.v[1].y - rord.v[0].y);
+        yzls = (rord.v[2].z - rord.v[0].z) / (rord.v[2].y - rord.v[0].y);
     }
     //printf("Slope top rs:%f ls:%f\n", rs, ls);
 
     for ( end = pt = rord.v[0]; pt.y <= rord.v[1].y; pt.y++ )
     {
-        if ( end.x > pt.x )
+        if ( end.x > pt.x && pt.y >= 0 && pt.y < SCREEN_HEIGHT )
         {
             // Draw line
             for ( i = pt.x; i <= end.x; i++ )
             {
-                uv_o = bary2cart(uv, cart2bary(&rord, i, pt.y));
-                setPixel( surf, i, pt.y, getPixel( uvsurf, uv_o.x, uv_o.y ) );
-                //setPixel( surf, i, pt.y, 0x0000FF );
+                float zd = pt.z;
+                if ( zd > z_depth[i][(int)pt.y] && i >= 0 && i < SCREEN_WIDTH ) {
+                    uv_o = bary2cart(uv, cart2bary(&rord, i, pt.y));
+                    setPixel( surf, i, pt.y, getPixel( uvsurf, uv_o.x, uv_o.y ) );
+                    //setPixel( surf, i, pt.y, 0x0000FF );
+                    z_depth[i][(int)pt.y] = zd;
+                }
+                zd += xzs;
             }
         }
 
         pt.x += ls;  end.x += rs;
+        pt.z += yzls; end.z += yzrs;
+        xzs = (pt.z - end.z) / (pt.x - end.x);
     }
 
     // Draw bottom half of triangle
+    // Get Left and Right slopes
     if ( cx < rord.v[1].x )
     {
         ls  = (rord.v[0].x - rord.v[2].x) / (rord.v[0].y - rord.v[2].y);
         rs  = (rord.v[1].x - rord.v[2].x) / (rord.v[1].y - rord.v[2].y);
+        yzls = (rord.v[0].z - rord.v[2].z) / (rord.v[0].y - rord.v[2].y);
+        yzrs = (rord.v[1].z - rord.v[2].z) / (rord.v[1].y - rord.v[2].y);
     }
     else
     {
         rs  = (rord.v[0].x - rord.v[2].x) / (rord.v[0].y - rord.v[2].y);
         ls  = (rord.v[1].x - rord.v[2].x) / (rord.v[1].y - rord.v[2].y);
+        yzrs = (rord.v[0].z - rord.v[2].z) / (rord.v[0].y - rord.v[2].y);
+        yzls = (rord.v[1].z - rord.v[2].z) / (rord.v[1].y - rord.v[2].y);
     }
     //printf("Slope bottom rs:%f ls:%f\n", rs, ls);
     for ( end = pt = rord.v[2]; pt.y >= rord.v[1].y; pt.y-- )
     {
-        if ( end.x > pt.x )
+        if ( end.x > pt.x && pt.y >= 0 && pt.y < SCREEN_HEIGHT )
         {
             // Draw line
             for ( i = pt.x; i <= end.x; i++ )
             {
-                uv_o = bary2cart(uv, cart2bary(&rord, i, pt.y));
-                setPixel( surf, i, pt.y, getPixel( uvsurf, uv_o.x, uv_o.y ) );
+                float zd = pt.z;
+                if ( zd > z_depth[i][(int)pt.y] && i >= 0 && i < SCREEN_WIDTH ) {
+                    uv_o = bary2cart(uv, cart2bary(&rord, i, pt.y));
+                    setPixel( surf, i, pt.y, getPixel( uvsurf, uv_o.x, uv_o.y ) );
+                    //setPixel( surf, i, pt.y, 0xFF00FF );
+                    z_depth[i][(int)pt.y] = zd;
+                }
+                zd += xzs;
                 //printf("UV: u: %f v: %f u: %f v: %f\n", test.x, test.y, uv_o.x, uv_o.y);
-                //setPixel( surf, i, pt.y, 0xFF0000 );
             }
         }
-
+        
         pt.x -= ls;  end.x -= rs;
+        pt.z -= yzls; end.z -= yzrs;
+        xzs = (pt.z - end.z) / (pt.x - end.x);
     }
 }
